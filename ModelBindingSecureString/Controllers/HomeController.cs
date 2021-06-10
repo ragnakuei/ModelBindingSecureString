da4744605dec638350ea8606c1abf3290ccf2071
Kuei <ragnakuei@livemail.tw>
2021-06-10 09:01:20 +0800

    add SecureString Sample

--- ModelBindingSecureString/Controllers/HomeController.cs
+++ ModelBindingSecureString/Controllers/HomeController.cs
@@ -3,9 +3,13 @@ using Microsoft.Extensions.Logging;
 using ModelBindingSecureString.Models;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Diagnostics;
 using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 
 namespace ModelBindingSecureString.Controllers
 {
@@ -18,9 +22,18 @@ namespace ModelBindingSecureString.Controllers
             _logger = logger;
         }
 
+        [HttpGet]
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(ViewModel vm)
+        {
+            var plainText = SecureStringToString(vm.Password);
+
+            return View(vm);
         }
 
         public IActionResult Privacy()
@@ -32,6 +45,53 @@ namespace ModelBindingSecureString.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        /// <summary>
+        /// 把 SecureString 轉回 PlainText
+        /// </summary>
+        private string SecureStringToString(SecureString value)
+        {
+            IntPtr valuePtr = IntPtr.Zero;
+            try
+            {
+                valuePtr = Marshal.SecureStringToGlobalAllocUnicode(value);
+                return Marshal.PtrToStringUni(valuePtr);
+            }
+            finally
+            {
+                Marshal.ZeroFreeGlobalAllocUnicode(valuePtr);
+            }
+        }
+    }
+
+    public class ViewModel
+    {
+        public string Account { get; set; }
+
+        [DataType(DataType.Password)]
+        [BindProperty(BinderType = typeof(SecureStringBinder))]
+        public SecureString Password { get; set; }
+    }
+
+    public class SecureStringBinder : IModelBinder
+    {
+        public Task BindModelAsync(ModelBindingContext bindingContext)
+        {
+            var valueProviderResult = bindingContext.ValueProvider.GetValue(bindingContext.ModelName);
+
+            var value = valueProviderResult.FirstValue;
+
+            var propertyValue = new SecureString();
+
+            foreach (var c in value)
+            {
+                propertyValue.AppendChar(c);
+            }
+
+            bindingContext.Result = ModelBindingResult.Success(propertyValue);
+
+            return Task.CompletedTask;
         }
     }
 }
